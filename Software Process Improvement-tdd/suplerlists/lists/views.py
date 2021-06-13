457dbfcf3deaca895528d59953c5885a48a1f70f
xianfei <979719361@qq.com>
2021-06-13 17:19:50 +0800

    add hadoop experiment pom.xml

--- Software Process Improvement-tdd/suplerlists/lists/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def home_page(request):
-	return render(request,'home.html',{
-		'new_item_text':request.POST.get('item_text',''),
-	})
-	#pass
